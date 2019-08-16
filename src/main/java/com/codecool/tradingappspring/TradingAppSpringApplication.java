e92fa0f1a8aa8d01e837d8e0657c018f7e5940ec
Szabó Gergő <Szabogergo1992@gmail.com>
2019-08-16 14:51:09 +0200

    spring project setup done

---
+++ src/main/java/com/codecool/tradingappspring/TradingAppSpringApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.tradingappspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingAppSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradingAppSpringApplication.class, args);
+    }
+
+}
